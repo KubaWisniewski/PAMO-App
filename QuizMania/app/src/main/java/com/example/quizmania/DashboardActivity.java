c088ed2ebcc13b9952b1df09a88e6a192b143925
Daniel Belter <danielbelter96@gmail.com>
2020-01-20 00:08:21 +0100

    Add User profile and ranking functionality

--- QuizMania/app/src/main/java/com/example/quizmania/DashboardActivity.java
+++ QuizMania/app/src/main/java/com/example/quizmania/DashboardActivity.java
@@ -1,6 +1,9 @@
 package com.example.quizmania;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,14 +17,31 @@ import retrofit2.Response;
 import static com.example.quizmania.LoginActivity.token;
 
 public class DashboardActivity extends AppCompatActivity {
+    public User user;
+    public Button profileButton, rankingButton;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_activity);
+        profileButton = findViewById(R.id.button_profile);
+        rankingButton = findViewById(R.id.ranking_button_id);
 
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanuchProfileActivity();
+            }
+        });
+        rankingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lanuchRankingActivity();
+            }
+        });
         Api.getInstance().getUserService().checkMe(token).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                User user = response.body();
+                user = response.body();
                 System.out.println(user.toString());
             }
 
@@ -31,5 +51,13 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
     }
+    private void lanuchProfileActivity(){
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
+    }
 
+    private void lanuchRankingActivity(){
+        Intent intent = new Intent(this, RankingActivity.class);
+        startActivity(intent);
+    }
 }
